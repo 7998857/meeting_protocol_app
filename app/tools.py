4c103679679e8a93e5e05b09ed9d6e707b1c7757
Peter Kettmann <p.kettmann@kaitos.ai>
2024-12-14 15:42:48 +0100

    some cleanup

--- app/tools.py
+++ app/tools.py
@@ -1,25 +1,16 @@
 import logging
+import os
+
+import assemblyai as aai
+from pydub import AudioSegment
 
 from app import db
 from config import Config
 from app.models import Participants, Meetings, Transcriptions, Protocols
 
-from flask import copy_current_request_context
-import os
-
-
-from functools import wraps
-from queue import Queue
 
 logger = logging.getLogger(__name__)
 logger.setLevel(Config.LOG_LEVEL)
-
-
-
-import argparse
-import os
-import assemblyai as aai
-from pydub import AudioSegment
 
 aai.settings.api_key = Config.ASSEMBLYAI_API_KEY
 transcriber = aai.Transcriber()
